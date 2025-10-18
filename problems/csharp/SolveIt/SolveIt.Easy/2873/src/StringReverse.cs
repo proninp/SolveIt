e8a76c4440a7762b8b31b3431e3c165565794296
proninp <velninpapro@gmail.com>
2025-10-18 10:45:17 +0300

    C# 2873

---
+++ problems/csharp/SolveIt/SolveIt.Easy/2873/src/StringReverse.cs
@@ -0,0 +1,11 @@
+﻿namespace SolveIt.Easy._2873.src;
+
+public class StringReverse
+{
+    public void ReverseString(string[] s) {
+        for (var i = 0; i < s.Length / 2; i++)
+        {
+            (s[i], s[s.Length - 1 - i]) = (s[s.Length - 1 - i], s[i]);
+        }
+    }
+}
