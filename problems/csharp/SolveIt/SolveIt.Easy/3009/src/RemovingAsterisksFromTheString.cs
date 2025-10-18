fec879c1d2005bf84a814a29bdcdeaec6069319b
proninp <velninpapro@gmail.com>
2025-10-18 10:25:21 +0300

    C# 3009

---
+++ problems/csharp/SolveIt/SolveIt.Easy/3009/src/RemovingAsterisksFromTheString.cs
@@ -0,0 +1,23 @@
+﻿namespace SolveIt.Easy._3009;
+
+public sealed class RemovingAsterisksFromTheString
+{
+    public string RemoveStars(string s) {
+        var stack = new Stack<char>();
+        foreach (var ch in s)
+        {
+            if (ch == '*')
+            {
+                if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+            else
+            {
+                stack.Push(ch);
+            }
+        }
+        return string.Concat(stack.Reverse());
+    }
+}
