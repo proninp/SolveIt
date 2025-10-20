0835d67d4ebf5f24282760e0ed0ee1c39efa67e3
proninp <velninpapro@gmail.com>
2025-10-20 22:44:44 +0300

    C# 3019

---
+++ problems/csharp/SolveIt/SolveIt.Easy.Tests/3019/TransferZerosTest.cs
@@ -0,0 +1,35 @@
+﻿using SolveIt.Easy._3019.src;
+
+namespace SolveIt.Easy.Tests._3019;
+
+public class TransferZerosTest
+{
+    [Fact]
+    public void MoveZeroesTest_One()
+    {
+        /// Arrange
+        var actual = new[] { 0, 1, 2, 0, 3 };
+        var expected = new[] { 1, 2, 3, 0, 0 };
+        
+        Test(actual, expected);
+    }
+    
+    [Fact]
+    public void MoveZeroesTest_Two()
+    {
+        /// Arrange
+        var actual = new[] { 1, 2, 3, 4, 5 };
+        var expected = new[] { 1, 2, 3, 4, 5 };
+        
+        Test(actual, expected);
+    }
+
+    private void Test(int[] actual, int[] expected)
+    {
+        /// Act
+        TransferZeros.MoveZeroes(actual);
+        
+        /// Assert
+        Assert.Equal(actual, expected);
+    }
+}
---
+++ problems/csharp/SolveIt/SolveIt.Easy/3019/src/TransferZeros.cs
@@ -0,0 +1,21 @@
+﻿namespace SolveIt.Easy._3019.src;
+
+public sealed class TransferZeros
+{
+    public static void MoveZeroes(int[] nums)
+    {
+        var insert = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                if (insert != i)
+                {
+                    nums[insert] = nums[i];
+                    nums[i] = 0;
+                }
+                insert++;
+            }
+        }
+    }
+}
