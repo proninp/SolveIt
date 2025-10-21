f671963acc53d04d98286bcce8cb5a68ec48a55c
proninp <velninpapro@gmail.com>
2025-10-21 23:27:25 +0300

    C# 2858

---
+++ problems/csharp/SolveIt/SolveIt.Easy/2858/src/MissingNumberSolution.cs
@@ -0,0 +1,8 @@
+﻿namespace SolveIt.Easy._2858.src;
+
+public sealed class MissingNumberSolution
+{
+    public int MissingNumber(int[] nums) {
+        return nums.Length * (nums.Length + 1) / 2 - nums.Sum();
+    }
+}
