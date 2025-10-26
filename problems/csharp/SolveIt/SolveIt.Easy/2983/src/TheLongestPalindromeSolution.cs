c474900aeb269da8840dda71eceb2ff3ae28baf4
proninp <velninpapro@gmail.com>
2025-10-26 21:53:57 +0300

    C# 2983

---
+++ problems/csharp/SolveIt/SolveIt.Easy/2983/src/TheLongestPalindromeSolution.cs
@@ -0,0 +1,28 @@
+﻿namespace SolveIt.Easy._2983.src;
+
+public class TheLongestPalindromeSolution
+{
+    public int LongestPalindrome(string s)
+    {
+        var vocabulary = new Dictionary<char, int>();
+        foreach (var c in s)
+        {
+            vocabulary.TryAdd(c, 0);
+            vocabulary[c]++;
+        }
+
+        var pairs = 0;
+        var isSingleExists = false;
+        foreach (var kvp in vocabulary)
+        {
+            pairs += kvp.Value > 1 ? kvp.Value / 2 : 0;
+            ;
+            if (kvp.Value % 2 != 0)
+            {
+                isSingleExists = true;
+            }
+        }
+
+        return pairs * 2 + (isSingleExists ? 1 : 0);
+    }
+}
