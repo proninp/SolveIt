c7d354f1680f50e37fc78e88a5dab6ecab55c14f
proninp <velninpapro@gmail.com>
2025-10-30 18:18:23 +0300

    C# 3036

---
+++ problems/csharp/SolveIt/SolveIt.Easy/3036/src/TribonacciSolution.cs
@@ -0,0 +1,17 @@
+﻿namespace SolveIt.Easy._3036.src;
+
+public class TribonacciSolution
+{
+    public int Tribonacci(int n)
+    {
+        int[] current = [0, 1, 1];
+        for (var i = 3; i <= n; i++)
+        {
+            var next = current.Sum();
+            current[0] = current[1];
+            current[1] = current[2];
+            current[2] = next;
+        }
+        return n < 3 ? current[n] : current[^1];
+    }
+}
