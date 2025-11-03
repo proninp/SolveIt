e65a2e3c8609e7481fc8a073b96be9ff14510d9c
proninp <velninpapro@gmail.com>
2025-11-03 11:48:40 +0300

    C# 2866

---
+++ problems/csharp/SolveIt/SolveIt.Middle/2866/src/TheAngleBetweenTheHandsOfTheClock.cs
@@ -0,0 +1,20 @@
+﻿namespace SolveIt.Middle._2866.src;
+
+public class TheAngleBetweenTheHandsOfTheClock
+{
+    public double AngleClock(int hour, int minutes)
+    {
+        const int oneMinDegree = 6;
+        const int segmentsInOneHour = 5;
+        const int degreesPerHourSegment = 30;
+        const int circleDegrees = 360;
+        const double minInHour = 60.0;
+        
+        var minsDegree = oneMinDegree * minutes;
+        var hourDegree = (hour % 12) * segmentsInOneHour / minInHour * circleDegrees;
+        var hourDegreeRemains = minutes / minInHour * degreesPerHourSegment;
+        hourDegree += hourDegreeRemains;
+        var handsDiff = Math.Abs(minsDegree - hourDegree);
+        return Math.Min(handsDiff, circleDegrees - handsDiff);
+    }
+}
