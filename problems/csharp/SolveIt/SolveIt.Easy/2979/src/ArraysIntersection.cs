5380bc6fd057cfce9771846993e51b489c9acaac
proninp <velninpapro@gmail.com>
2025-11-04 09:52:55 +0300

    C# 2979

---
+++ problems/csharp/SolveIt/SolveIt.Easy/2979/src/ArraysIntersection.cs
@@ -0,0 +1,19 @@
+﻿namespace SolveIt.Easy._2979.src;
+
+public sealed class ArraysIntersection
+{
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        var frequency = new Dictionary<int, int>();
+        foreach (var num in nums1)
+        {
+            frequency.TryAdd(num, 1);
+        }
+        foreach (var num in nums2)
+        {
+            frequency.TryAdd(num, 2);
+            frequency[num] |= 2;
+
+        }
+        return frequency.Keys.Where(x => frequency[x] == 3).ToArray();
+    }
+}
