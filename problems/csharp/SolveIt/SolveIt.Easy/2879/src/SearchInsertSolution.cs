7f74719f3b96457a4b85f80a2fb6f5408f07c33e
proninp <velninpapro@gmail.com>
2025-11-07 08:09:14 +0300

    C# 2879

---
+++ problems/csharp/SolveIt/SolveIt.Easy/2879/src/SearchInsertSolution.cs
@@ -0,0 +1,28 @@
+﻿namespace SolveIt.Easy._2879.src;
+
+public sealed class SearchInsertSolution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+        var mid = 0;
+        while (left <= right)
+        {
+            mid = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return nums[mid] < target ? mid + 1 : mid;
+    }
+}
