55abf1b030e5b1f398306c3b9ca4e8671dcf38fe
proninp <velninpapro@gmail.com>
2025-11-12 08:57:30 +0300

    C# 2974

---
+++ problems/csharp/SolveIt/SolveIt.Easy/2974/src/MaxPowerSolution.cs
@@ -0,0 +1,18 @@
+﻿namespace SolveIt.Easy._2974.src;
+
+public class MaxPowerSolution
+{
+    public int MaxPower(string s) {
+        var maxPower = 1;
+        var power = 1;
+        for (var i = 1; i < s.Length; i++)
+        {
+            power = s[i] == s[i - 1] ? power + 1 : 1; 
+            if (maxPower < power)
+            {
+                maxPower = power;
+            }
+        }
+        return maxPower;
+    }
+}
