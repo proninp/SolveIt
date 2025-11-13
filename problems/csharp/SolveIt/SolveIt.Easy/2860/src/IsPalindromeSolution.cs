abe2b779c8cb63ef121af2f053d4360e85d80f41
proninp <velninpapro@gmail.com>
2025-11-13 08:33:02 +0300

    C# 2860

---
+++ problems/csharp/SolveIt/SolveIt.Easy/2860/src/IsPalindromeSolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace SolveIt.Easy._2860.src;
+
+public class IsPalindromeSolution
+{
+    public bool IsPalindrome(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (left < right)
+        {
+            while (left < right && !char.IsLetterOrDigit(s[left]))
+                left++;
+            while (left < right && !char.IsLetterOrDigit(s[right]))
+                right--;
+            if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
