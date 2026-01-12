ca1e92da1278e9fcdbeec0436a7eae8ff0f27d19
proninp <velninpapro@gmail.com>
2026-01-12 09:16:49 +0300

    C# 3208

---
+++ problems/csharp/SolveIt/SolveIt.Easy/3208/src/ClimbStairsSolution.cs
@@ -0,0 +1,15 @@
+﻿namespace SolveIt.Easy._3208;
+
+public class ClimbStairsSolution
+{
+    public int ClimbStairs(int n)
+    {
+        var a = 1;
+        var b = 1;
+        for (var i = 2; i <= n; i++)
+        {
+            (a, b) = (a + b, a);
+        }
+        return a;
+    }
+}
