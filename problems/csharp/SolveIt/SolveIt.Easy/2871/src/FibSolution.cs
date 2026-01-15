0f5110d8716627e5932d41dc1decc1b7b30cc912
proninp <velninpapro@gmail.com>
2026-01-15 08:53:27 +0300

    C# 2871

---
+++ problems/csharp/SolveIt/SolveIt.Easy/2871/src/FibSolution.cs
@@ -0,0 +1,16 @@
+﻿namespace SolveIt.Easy._2871;
+
+public class FibSolution
+{
+    public int Fib(int n) {
+        if (n < 2)
+            return n;
+        var b = 0;
+        var a = 1;
+        for (var i = 2; i <= n; i++)
+        {
+            (b , a) = (a, a + b);
+        }
+        return a;
+    }
+}
