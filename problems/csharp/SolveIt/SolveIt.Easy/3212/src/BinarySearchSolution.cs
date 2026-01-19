5e0745baa2b0600849b8df8e8bf066d2f94b2c45
proninp <velninpapro@gmail.com>
2026-01-19 22:08:24 +0300

    C# 3212

---
+++ problems/csharp/SolveIt/SolveIt.Easy/3212/src/BinarySearchSolution.cs
@@ -0,0 +1,20 @@
+﻿namespace SolveIt.Easy._3212;
+
+public class BinarySearchSolution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            var mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+}
