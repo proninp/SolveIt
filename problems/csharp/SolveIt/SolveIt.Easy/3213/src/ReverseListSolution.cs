8c87125deb7db8af788866115094868e55147646
proninp <velninpapro@gmail.com>
2026-01-20 08:51:57 +0300

    C# 3213

---
+++ problems/csharp/SolveIt/SolveIt.Easy/3213/src/ReverseListSolution.cs
@@ -0,0 +1,33 @@
+﻿namespace SolveIt.Easy._3213;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseListSolution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        var current = head;
+        ListNode prev = null;
+        ListNode next;
+
+        while (current != null)
+        {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        return prev;
+    }
+}
