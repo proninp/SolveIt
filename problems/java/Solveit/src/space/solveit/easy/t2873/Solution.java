f1692e6e02ea03b20a080592c86ad098b548a76f
proninp <velninpapro@gmail.com>
2026-03-28 19:53:59 +0300

    Java 2873

---
+++ problems/java/Solveit/src/space/solveit/easy/t2873/Solution.java
@@ -0,0 +1,13 @@
+﻿package space.solveit.easy.t2873;
+
+// https://solvit.space/coding/2873
+
+public class Solution {
+    public void reverseString(char[] s) {
+        for (var i = 0; i < s.length/2; i++) {
+            var t = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = t;
+        }
+    }
+}
