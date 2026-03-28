16f486c4b06ac59ba9f311e1c10d2a394cdb435f
proninp <velninpapro@gmail.com>
2026-03-28 19:53:46 +0300

    Java 2858

---
+++ problems/java/Solveit/src/space/solveit/easy/t2858/Solution.java
@@ -0,0 +1,12 @@
+﻿package space.solveit.easy.t2858;
+
+// https://solvit.space/coding/2858
+
+import java.util.Arrays;
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        var s = nums.length * (nums.length + 1) / 2;
+        return s - Arrays.stream(nums).sum();
+    }
+}
