0d0cf5ee5a83c64acaf38ec0077467c9482b03ca
proninp <velninpapro@gmail.com>
2026-03-28 16:00:23 +0300

    C# 3219

---
+++ problems/csharp/SolveIt/SolveIt.Easy/3219/src/LRUCache.cs
@@ -0,0 +1,43 @@
+﻿namespace SolveIt.Easy._3219;
+
+public class LRUCache
+{
+
+    private readonly Dictionary<int, LinkedListNode<(int key, int value)>> _map;
+    private readonly LinkedList<(int key, int value)> _cache;
+    private readonly int _limit;
+    
+    public LRUCache(int capacity) {
+        _limit = capacity;
+        _map = new(capacity);
+        _cache = new();
+    }
+    
+    public int Get(int key)
+    {
+        if (!_map.TryGetValue(key, out var node))
+            return -1;
+        
+        _cache.Remove(node);
+        _cache.AddFirst(node);
+        
+        return node.Value.value;
+    }
+    
+    public void Put(int key, int value) {
+        if (_map.TryGetValue(key, out var node))
+        {
+            _cache.Remove(node);
+        }
+        else if (_cache.Count == _limit)
+        {
+            var lru = _cache.Last!;
+            _cache.Remove(lru);
+            _map.Remove(lru.Value.key);
+        }
+
+        var newNode = new LinkedListNode<(int key, int value)>((key, value));
+        _cache.AddFirst(newNode);
+        _map[key] = newNode;
+    }
+}
